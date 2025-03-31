f6b8efaa032e4df78e15364a76c396ad2225d4b8
madeline <madeline.toledo.manriquez@alumnos.sip.cl>
2025-03-31 12:40:37 -0300

    python

---
+++ control-De flujo/PH.py
@@ -0,0 +1,15 @@
+#madeline
+
+ph=int(input("ingresa el nivel de ph(0-14):"))
+
+if ph > 7:
+ 
+  print("Ph")
+
+elif basico < 7:
+         print("ACIDO")
+else:
+         print("neutro")
+
+
+   
---
+++ control-De flujo/Promedio.py
@@ -0,0 +1,15 @@
+#Madeline 31-03-2024
+nota=40
+if nota>70:
+    print(" PASO👌")
+    elif nota>60:
+    
+    print("paso")
+    elif nota>50:
+
+    print("vas por un buen camino🚶🏻")
+    elif nota>40:
+    
+    print("Nota es Nota Igual es Azul📘")
+    else
+    print("No te rindas, cada Error es una Oportunidad⚠️")
